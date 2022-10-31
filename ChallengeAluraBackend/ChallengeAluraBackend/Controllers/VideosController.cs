5039988638bb6682c0ef231d6371efec2695df4a
Erick Aoyagui <erickaoyagui@hotmail.com>
2022-10-30 23:00:05 -0300

    Commit Inicial

---
+++ ChallengeAluraBackend/ChallengeAluraBackend/Controllers/VideosController.cs
@@ -0,0 +1,26 @@
+﻿using ChallengeAluraBackend.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChallengeAluraBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VideosController : ControllerBase
+    {
+        private static List<Video> videos = new List<Video>();
+        [HttpGet]
+        public IActionResult RecuperaVideo()
+        {
+            return Ok(videos);
+        }
+
+        [HttpPost]
+        public IActionResult AdicionaVideo ([FromBody]Video video)
+        {
+            videos.Add(video);
+            return Ok(video);
+        }
+
+    }
+}
---
+++ ChallengeAluraBackend/ChallengeAluraBackend/Models/Video.cs
@@ -0,0 +1,11 @@
+﻿namespace ChallengeAluraBackend.Models
+{
+    public class Video
+    {
+        public int id { get; set; }
+        public string titulo { get; set; }
+        public string descricao { get; set; }
+        public string url { get; set; }
+        public int categoria { get; set; }
+    }
+}
